d6fd235a7a42320965a249dae447c582f7b32b2d
duyankim <arbolkim@gmail.com>
2021-05-23 19:28:21 +0900

    10808 : 단어에서 알파벳 개수 구하기

---
+++ src/baekjoon/Alphabet10808.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Alphabet10808 {
+
+	public static void main(String[] args) {
+		int[] alphabet = new int[26];	
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+
+		for (int i = 0; i < input.length(); i++) {
+			alphabet[(int)(input.charAt(i))-97] += 1;
+		}
+		for (int n : alphabet) {
+			System.out.print(n + " ");
+		}
+	}
+}
