50e5f69278897bd9583c0d153971cd29d4e27f18
duyankim <arbolkim@gmail.com>
2021-09-19 22:15:47 +0900

    [Sort]프로그래머스 - H-index

---
+++ src/programmers/sort/HIndex.java
@@ -0,0 +1,27 @@
+package programmers.sort;
+
+import java.util.*;
+
+public class HIndex {
+    public static void main(String[] args) {
+        int[] citations = {3, 0, 6, 1, 5};
+        solution(citations);
+    }
+
+    public static int solution(int[] citations) {
+        int answer = 0;
+
+        Arrays.sort(citations);
+
+        for (int i = 0; i < citations.length; i++) {
+            int h = citations.length - i;
+
+            if (citations[i] >= h) {
+                answer = h;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
